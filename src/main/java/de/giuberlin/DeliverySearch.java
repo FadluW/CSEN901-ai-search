3b9183dd527e37f8098ee557865fd60f5cf40dfe
omarafekry <67840386+omarafekry@users.noreply.github.com>
2024-11-30 23:23:28 +0100

    adds traffic parsing

--- src/main/java/de/giuberlin/DeliverySearch.java
+++ src/main/java/de/giuberlin/DeliverySearch.java
@@ -1,11 +1,14 @@
 package de.giuberlin;
 
+import de.giuberlin.grid.Grid;
+import de.giuberlin.grid.types.GridObject;
 import de.giuberlin.search.strategies.Strategy;
 import de.giuberlin.search.strategies.StrategyCode;
-import de.giuberlin.grid.types.GridObject;
+import java.awt.*;
 
 public class DeliverySearch extends GenericSearch implements DeliverySearchInterface {
 
+    Grid grid;
     @Override
     public String GenGrid() {
 
@@ -22,13 +25,27 @@ public class DeliverySearch extends GenericSearch implements DeliverySearchInter
     public String plan(String initialState, String traffic, String strategy, Boolean visualize) {
         // Create grid to use
 
-        // Add traffic between each node
+        addTraffic(traffic);
 
         // Map search strategy
         Strategy searchStrategy = StrategyCode.valueOf(strategy.toUpperCase()).getStrategy();
 
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'plan'");
+    }
+
+    private void addTraffic(String traffic){
+        String[] segments = traffic.split(";");
+        for (String segment : segments) {
+            String[] segmentValues = segment.split(",");
+            int trafficValue = Integer.parseInt(segmentValues[4]);
+            if (trafficValue < 1)
+                continue;
+
+            Point source = new Point(Integer.parseInt(segmentValues[0]), Integer.parseInt(segmentValues[1]));
+            Point destination = new Point(Integer.parseInt(segmentValues[2]), Integer.parseInt(segmentValues[3]));
+            grid.setTraffic(source, destination, trafficValue);
+        }
     }
 
 
