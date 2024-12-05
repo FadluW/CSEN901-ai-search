4d1907bca72ce8d51b6bb43701fe6ed44cdaf147
FadluW <fadlu.w@gmail.com>
2024-12-06 00:21:12 +0100

    * Init DeliveryPlanner

--- src/main/java/de/giuberlin/DeliveryPlanner.java
+++ src/main/java/de/giuberlin/DeliveryPlanner.java
@@ -1,5 +1,25 @@
 package de.giuberlin;
 
 public class DeliveryPlanner {
+    private DeliverySearch deliverySearch;
+    private String initialState;
+    private String traffic;
+    private String strategy;
+    private boolean visualize;
 
+    private DeliveryPlanner(String initialState, String traffic, String strategy, boolean visualize) {
+        this.deliverySearch = new DeliverySearch();
+        this.initialState = initialState;
+        this.traffic = traffic;
+        this.strategy = strategy;
+        this.visualize = visualize;
+    }
+
+    private DeliveryPlanner(String initialState, String traffic, boolean visualize) {
+        this.deliverySearch = new DeliverySearch();
+        this.initialState = initialState;
+        this.traffic = traffic;
+        this.strategy = null;
+        this.visualize = visualize;
+    }
 }
